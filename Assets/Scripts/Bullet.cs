cfa52d898c0b46c3b1cb79d932228fc96160bf27
crazy78cyxapuk <olezhki.net@gmail.com>
2021-02-08 23:39:48 +0300

    изменены цвета объектов; переписана логика соприкосновения пули и мишени

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,7 +9,7 @@ public class Bullet : MonoBehaviour
     [HideInInspector] public Transform target;
     [HideInInspector] public bool isInit = false;
 
-    private Vector3 pointFlew1, pointFlew2, pointFlewCenter;
+    private Vector3 pointFlewCenter;
     private float t = 0;
 
     private void Update()
@@ -22,18 +22,14 @@ public class Bullet : MonoBehaviour
             if (t <= 1f)
             {
                 transform.position = Spline.GetPointQuadro(transform.position, pointFlewCenter, target.position, t);
-                //t += 1f/(Time.deltaTime * speed);
                 t += 0.03f;
-                //Debug.Log(t);
             }
-            //transform.rotation = Quaternion.LookRotation(Bezier.GetFirstDerivative(P0.position, P1.position, P2.position, P3.position, t));
 
             transform.up = Vector3.Lerp(transform.up, target.position - transform.position, 0.5f);
 
             if(Vector3.Distance(transform.position, target.position) < 0.1f)
             {
                 Zeroing();
-                Camera.main.GetComponent<CameraShake>().Shake();
             }
         }
     }
@@ -44,23 +40,18 @@ public class Bullet : MonoBehaviour
         {
             Debug.Log("HUMAN");
         }
+
+        if (other.gameObject.CompareTag("Target"))
+        {
+            Camera.main.GetComponent<CameraShake>().Shake();
+        }
     }
 
     public void Initialization(Transform target)
     {
         this.target = target;
 
-        pointFlewCenter = new Vector3((transform.position.x + target.position.x) / 2f,
-            //Mathf.Max(transform.position.y, target.position.y) - Mathf.Min(transform.position.y, target.position.y) + transform.position.y,
-            (transform.position.y + target.position.y) / 1.2f,
-            (transform.position.z + target.position.z) / 2f);
-
-        pointFlew1 = new Vector3((transform.position.x + pointFlewCenter.x) / 2f, pointFlewCenter.y + transform.position.y, (transform.position.z + pointFlewCenter.z) / 2f);
-        pointFlew2 = new Vector3((target.position.x + pointFlewCenter.x) / 2f,  target.position.y - pointFlewCenter.y, (target.position.z + pointFlewCenter.z) / 2f);
-        
-        //Instantiate(this.gameObject, pointFlew1, Quaternion.identity);
-        //Instantiate(this.gameObject, pointFlew2, Quaternion.identity);
-        //Debug.LogError("Create pointFlew");
+        pointFlewCenter = new Vector3((transform.position.x + target.position.x) / 2f, (transform.position.y + target.position.y) / 1.2f, (transform.position.z + target.position.z) / 2f);
 
         isInit = true;
     }
